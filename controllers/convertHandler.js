46730ac1ef9008516871385a95cbd691bd5f1dae
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-12 22:37:17 +0800

    fix convert to string

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -108,7 +108,7 @@ function ConvertHandler() {
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result = `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)} `;
+    let result = `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
     
     return result;
   };
